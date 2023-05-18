a02a52b6f5e61794dcd311a2cee4cf79e2b12ee6
Martynas Narbutas <martynas.narbutas@deliveryhero.com>
2023-05-18 20:43:09 +0200

    fixed bug

--- v2/geom/square.go
+++ v2/geom/square.go
@@ -1,15 +1,15 @@
 package geom
 
 type Square struct {
-	side float64
+	Side float64
 }
 
 func (s Square) Perimeter() float64 {
-	return s.side * 4.0
+	return s.Side * 4.0
 }
 
 func (s Square) Area() float64 {
-	return s.side * s.side
+	return s.Side * s.Side
 }
 
 func SumAreas(a float64, b float64) float64 {
